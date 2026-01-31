8cdb58a6a5dd4dc4cbdd15659ef37a79238d6b63
MD Delwar Hossain Dulal <mddelwarhossaindulal1@gmail.com>
2026-02-01 01:42:36 +0600

    final

--- docs/js/btn.js
+++ docs/js/btn.js
@@ -1,4 +1,4 @@
-// ?page
+// ?page Home to Blog , ,,,, Blog to Home.
 function homeFunction() {
   location.replace("index.html");
 }
@@ -9,44 +9,11 @@ function blogFunction() {
 //! Donation & History Bbt hh/
 // showDonation হলো nav id
 document.getElementById("showDonation").addEventListener("click", function () {
-  showSectionById("mainContent");
-  showColorById("showDonation");
+  showSectionById("mainContent"); // section hide
+  showDonationColorById("showDonation"); // Donation Color btn
 });
 // showHistory হলো nav id
 document.getElementById("showHistory").addEventListener("click", function () {
-  showSectionById("history-section");
-  showColorById("showHistory");
+  showSectionById("history-section"); // section hide
+  showHistoryColorById("showHistory"); // History Color btn
 });
-
-// function showColorById(id) {
-//   document
-//     .getElementById("showDonation")
-//     .classList.add("bg-[#F8F8F8],text-[#111111b3]");
-//   document
-//     .getElementById("showHistory")
-//     .classList.add("bg-[#F8F8F8], text-[#111111b3]");
-//   document.getElementById(id).classList.add("bg-lime-400", "text-black");
-// }
-
-// const donate = document.getElementById("showDonation");
-// const history = document.getElementById("showHistory");
-
-// history.addEventListener("click", function () {
-//   history.classList.add("bg-[#B4F461]", "text-black");
-
-//   history.classList.remove("text-[#111111b3]");
-
-//   donate.classList.remove("bg-[#B4F461]");
-
-//   donate.classList.add("text-[#111111b3]");
-// });
-
-// donate.addEventListener("click", function () {
-//   donate.classList.add("bg-[#B4F461]", "text-black");
-
-//   donate.classList.remove("text-[#111111b3]");
-
-//   history.classList.remove("bg-[#B4F461]", "text-black");
-
-//   history.classList.add("text-[#111111b3]");
-// });
--- docs/js/utility.js
+++ docs/js/utility.js
@@ -1,4 +1,3 @@
-console.log("ut");
 // ইনপুট ফিল্ড থেকে সংখ্যা বের করবে
 function getInputFieldValueById(id) {
   const inputValue = document.getElementById(id).value;
@@ -19,12 +18,28 @@ function showSectionById(id) {
   document.getElementById(id).classList.remove("hidden");
 }
 
-// function showColorById(id) {
-//   document
-//     .getElementById("showDonation")
-//     .classList.add("bg-[#F8F8F8],text-[#111111b3]");
-//   document
-//     .getElementById("showHistory")
-//     .classList.add("bg-[#F8F8F8], text-[#111111b3]");
-//   document.getElementById(id).classList.add("bg-lime-400", "text-black");
-// }
+//! >>>>>>>>>>>>>>>>>>>>>>>>>>>>>  Donation বাটনের জন্য ফাংশন <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+function showDonationColorById(id) {
+  document
+    .getElementById("showDonation")
+    .classList.add("bg-[#B4F461]", "text-black");
+  document.getElementById("showDonation").classList.remove("text-[#111111b3]");
+  // history btn
+  document
+    .getElementById("showHistory")
+    .classList.remove("bg-[#B4F461]", "text-black");
+  document.getElementById("showHistory").classList.add("text-[#111111b3]");
+}
+
+// !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> History বাটনের জন্য ফাংশন <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+function showHistoryColorById(id) {
+  document
+    .getElementById("showHistory")
+    .classList.add("bg-[#B4F461]", "text-black");
+  document.getElementById("showHistory").classList.remove("text-[#111111b3]");
+  // *Donation btn
+  document
+    .getElementById("showDonation")
+    .classList.remove("bg-[#B4F461]", "text-black");
+  document.getElementById("showDonation").classList.add("text-[#111111b3]");
+}
